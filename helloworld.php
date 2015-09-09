6229c5dac9a35dfd96f01b548a121715271fb30f
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2015-09-09 12:25:09 +0300

    added lines of code

--- helloworld.php
+++ helloworld.php
@@ -3,3 +3,39 @@
 	$last_name = "Kirsima";
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+	$age = 19;
+	
+	//testib loogikat, juhul kui vanus on väikesm kui 18, siis kirjuta ""alaealine""
+	//muul juhul "täisealine"
+	
+	//if'i sisse loogikatehe
+	if($age < 18) {
+		//tõene
+		
+		echo "alaealine";
+	} else {
+		//väär
+		
+		echo "täisealine";
+		
+	}
+	
+?>
+<br>
+<?php
+
+	//vastavalt vanusele trükime nii mitu korda välja sõna "palju"
+	
+	for($i = 0; $i < $age; $i = $i + 1) {
+			
+			//tegevus mis kordub
+			echo "palju".$i." ";
+			
+	}
+
+	echo "õnne!"
+
+?>
