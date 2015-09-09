886af0eac8f24e576ef93ca4efc3b569b080194f
Raoul Kirsima <raoukir@greeny.cs.tlu.ee>
2015-09-09 13:13:09 +0300

    added lines of code

--- login.php
+++ login.php
@@ -1,10 +1,29 @@
 <?php
 
 	// LOGIN.PHP
+	//echo $_POST["email"];
+	$email_error = "";
+	$password_error = "";
 	
+	//kontrollime, et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST" ) {
+		
+		//echo "keegi vajutas nuppu";
+		//kontrollin, et email ei ole tühi
+		
+		
+		if ( empty($_POST["email"]) ) {
+			$email_error = "See väli on kohustuslik";
+		
+		}
+		
+		//kontrollin, et parool ei ole tühi
+		
+		if (empty($_POST["password"]) ) {
+			$password_error = "See väli on kohustuslik";
+		}
 
-
-
+	}
 ?>
 
 <html>
@@ -15,11 +34,12 @@
 
 <body>
 	<h2>Log in</h2>
-	
-		<input type="email" placeholder="e-mail"> <br><br>
-		<input type="password" placeholder="password"> <br><br>
-		<input type="submit" value="Log in">
 		
+		<form action="login.php" method="post" >
+			<input name="email" type="email" placeholder="e-mail"> <?php echo $email_error; ?> <br><br>
+			<input name="password" type="password" placeholder="password"> <?php echo $password_error; ?> <br><br>
+			<input type="submit" value="Log in">
+		</form>
 	
 	<h2>Create user</h2>
 	
